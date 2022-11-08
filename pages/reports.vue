0a8cf7b19cb7f6eb4945270d261290251d24d500
Antelinge <106556219+Antelinge@users.noreply.github.com>
2022-11-08 15:38:29 -0500

    Add files via upload

---
+++ pages/reports.vue
@@ -0,0 +1,89 @@
+<template>
+ <div class="row">
+
+  <div class="col-md-6">
+    <div class="card">
+      <!----> 
+      <div class="card-header">
+        <h4 class="card-title">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Selector de fecha y hora de inicio</font>
+          </font>
+        </h4>
+      </div> 
+            
+      <div class="card-body"> 
+        <div class="form-group">
+                <!----> 
+          <div class="mb-0">
+                  <!----> 
+            <div class="el-date-editor el-input el-input--prefix el-input--suffix el-date-editor--datetime">
+                    <!---->
+              <input type="text" autocomplete="off" name="" placeholder="Selector de fecha y hora" class="el-input__inner">
+                <span class="el-input__prefix">
+                  <i class="el-input__icon el-icon-date"></i>
+                        <!---->
+                </span>
+                <span class="el-input__suffix">
+                  <span class="el-input__suffix-inner">
+                   <i class="el-input__icon"></i>
+                              <!----><!----><!----><!---->
+                  </span>
+                              <!---->
+                </span>
+                              <!----><!---->
+            </div> 
+                              <!---->
+          </div> 
+                              <!----> 
+        </div>
+      </div> 
+                              <!---->  <!---->
+  </div>
+</div>
+
+<div class="col-md-6">
+    <div class="card">
+      <!----> 
+      <div class="card-header">
+        <h4 class="card-title">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Selector de fecha y hora de Final</font>
+          </font>
+        </h4>
+      </div> 
+            
+      <div class="card-body"> 
+        <div class="form-group">
+                <!----> 
+          <div class="mb-0">
+                  <!----> 
+            <div class="el-date-editor el-input el-input--prefix el-input--suffix el-date-editor--datetime">
+                    <!---->
+              <input type="text" autocomplete="off" name="" placeholder="Selector de fecha y hora" class="el-input__inner">
+                <span class="el-input__prefix">
+                  <i class="el-input__icon el-icon-date"></i>
+                        <!---->
+                </span>
+                <span class="el-input__suffix">
+                  <span class="el-input__suffix-inner">
+                   <i class="el-input__icon"></i>
+                              <!----><!----><!----><!---->
+                  </span>
+                              <!---->
+                </span>
+                              <!----><!---->
+            </div> 
+                              <!---->
+          </div> 
+                              <!----> 
+        </div>
+      </div> 
+                              <!---->  <!---->
+  </div>
+</div>
+
+</div>
+
+</template>
+
